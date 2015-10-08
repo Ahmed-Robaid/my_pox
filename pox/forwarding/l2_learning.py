4b90f4c5b0019e8fda2bddfff20508f1cbdc347a
Srini Seetharaman <seethara@stanford.edu>
2015-10-08 14:27:49 +1000

    fixes

--- pox/forwarding/l2_learning.py
+++ pox/forwarding/l2_learning.py
@@ -167,16 +167,12 @@ class LearningSwitch (object):
                   (packet.src, event.port, packet.dst, port))
         msg = of.ofp_flow_mod()
         msg.match = of.ofp_match.from_packet(packet, event.port)
-        msg.idle_timeout = 3
-        msg.hard_timeout = 3
+        msg.idle_timeout = 10
+        msg.hard_timeout = 30
         msg.actions.append(of.ofp_action_output(port = port))
         msg.data = event.ofp # 6a
         self.connection.send(msg)
-        # sleep(1)
-        # msg = of.ofp_flow_mod(command=of.OFPFC_DELETE)
-        # for connection in core.openflow.connections: # _connections.values() before betta
-        #     connection.send(msg)
-
+        
 class l2_learning (object):
   """
   Waits for OpenFlow switches to connect and makes them learning switches.
