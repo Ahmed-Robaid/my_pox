8ce0ca6f6096b9f396eda93f7895f7967baa0228
Srini Seetharaman <seethara@stanford.edu>
2015-10-13 15:53:22 +1000

    data

--- measure/time/ping_script.sh
+++ measure/time/ping_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-    for i in `seq 1 100`;
+    for i in `seq 1 30`;
     do
-        arp -d 10.0.0.50
-        output="$(ping -c1 10.0.0.50)"
-        sleep 2 
+        arp -d 10.0.0.80
+        output="$(ping -c1 10.0.0.80)"
+        sleep 12 
         echo $output | egrep -o "time=[0-9]+.[0-9]+"| egrep -o "[0-9]+.[0-9]+"
     done
 
